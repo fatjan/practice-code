85ce5f1bcb63343c9716d7c1b37ea6be464a2441
fatjan <fatma.janna.utp@gmail.com>
2022-09-30 07:15:48 +0700

    remove prints

--- python/stock.py
+++ python/stock.py
@@ -3,17 +3,12 @@ def maxProfit(prices):
         return 0
 
     buy = min(prices[: len(prices) - 1])
-    print("buy", buy)
     day_buy = prices.index(buy)
-    print("day_buy", day_buy)
 
     sells = prices[day_buy + 1 : len(prices)]
     sell = max(sells)
-    print("sell", sell)
-    print("sells", sells)
 
     day_sell = day_buy + sells.index(sell) + 1
-    print("day_sell", day_sell)
 
     profit = sell - buy
     if profit > 0 and day_buy < day_sell:

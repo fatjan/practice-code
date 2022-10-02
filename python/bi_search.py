d41b820b29597c1960fbc4c14f6fde34c37fbdf3
fatjan <fatma.janna.utp@gmail.com>
2022-10-02 10:22:26 +0700

    binary search iterative

--- python/bi_search.py
+++ python/bi_search.py
@@ -19,3 +19,24 @@ def binary_search_recursive(arr, num, left, right):
 arr = [1, 5, 9, 54, 74, 62, 115]
 print(binary_search_recursive(arr, 62, 0, len(arr) - 1))  # True
 print(binary_search_recursive(arr, 162, 0, len(arr) - 1))  # False
+
+
+def binary_search_iterative(arr, num):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+        if arr[mid] == num:
+            return True
+        elif num < arr[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return False
+
+
+arr = [1, 5, 9, 54, 74, 62, 115]
+print(binary_search_iterative(arr, 62))  # True
+print(binary_search_iterative(arr, 162))  # False

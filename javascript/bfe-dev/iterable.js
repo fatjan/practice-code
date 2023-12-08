59bf8d0ac90114c71ce6d54faa231553fdf1eda6
Fatma Janna <fatma.janna@quinyx.com>
2023-12-08 09:16:55 +0700

    iterable

---
+++ javascript/bfe-dev/iterable.js
@@ -0,0 +1,11 @@
+const entries = [1,2,3,4].entries()
+for (const [i, item] of entries) {
+  console.log(i)
+  console.log(item)
+  break;
+}
+for (const [i, item] of entries) {
+  console.log(i)
+  console.log(item)
+  break;
+}

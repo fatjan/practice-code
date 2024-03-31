8bf315a9104a3736e29a45614c903f9fff5f8f3f
Fatma Janna <fatma.janna@quinyx.com>
2024-03-31 10:42:08 +0700

    count_alternate_sub

---
+++ python/alternate.py
@@ -0,0 +1,25 @@
+def count_alternate_sub(nums):
+    len_arr = len(nums)
+    def is_alternating(arr):
+        len_arr = len(arr)
+        if len_arr == 1:
+            return True
+        for k in range(1, len_arr):
+            if arr[k] != arr[k-1]:
+                continue
+            else:
+                return False
+        return True
+    
+    count = 0
+    for i in range(len_arr):
+        for j in range(i+1, len_arr+1):
+            sub = nums[i:j]
+            if is_alternating(sub):
+                count += 1
+    
+    return count
+
+print(count_alternate_sub([1, 2, 3, 4, 5])) # 15
+print(count_alternate_sub([1, 2, 1, 2, 1])) # 7
+print(count_alternate_sub([0,1,1,1])) # 5

ccfbb9e196886d324d12664ef1f40a897dd2c1b9
Fatma Janna <fatma.janna@quinyx.com>
2024-11-10 19:32:24 +0700

    intersection of two arrays using hash map

---
+++ python/common-sense-guide/intersection.py
@@ -0,0 +1,22 @@
+def find_intersection(arr1, arr2):
+    if len(arr2) < len(arr1):
+        arr1, arr2 = arr2, arr1
+    
+    keep_arr2 = {}
+    for item in arr2:
+        keep_arr2[item] = True
+    
+    intersection = []
+    for item in arr1:
+        if item in keep_arr2:
+            intersection.append(item)
+    
+    return intersection
+
+# Given two arrays
+arr1 = [1, 2, 3, 4, 5]
+arr2 = [3, 4, 5, 6, 7]
+
+# Find the intersection of two arrays
+intersection = find_intersection(arr1, arr2)
+print("Intersection of two arrays:", intersection)
